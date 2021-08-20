fe98914db6641984c76a508ae38826de0bb1deb2
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2021-08-20 19:43:58 +0900

    Update version to 1.0.0!

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MipsAssembler.MixProject do
     [
       app: :mips_assembler,
       escript: escript_config(),
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
